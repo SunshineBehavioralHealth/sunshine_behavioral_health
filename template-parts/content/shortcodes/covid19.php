9a6ddfe7178f13ec396bf4c1a44724020f0b5dbf
Nathan Wilson <natewilson135@gmail.com>
2021-04-06 14:58:59 -0400

    Removed plus from covid sidebar and created css for cta

--- template-parts/content/shortcodes/covid19.php
+++ template-parts/content/shortcodes/covid19.php
@@ -4,11 +4,7 @@
         <div class="covid_sidebar_content_wrapper">
             <p>Do not allow COVID-19 to stop you from seeking the care you need.</p>
             <p>We are here to answer your questions and alleviate any concerns. Call us today.</p>
-            <a class="invocaNumber covid_sidebar_cta" href="tel:949-276-2886">ff949-276-2886</a>
+            <a class="invocaNumber covid_sidebar_cta" href="tel:949-276-2886">949-276-2886</a>
         </div>
-
-    </div>
-    <div class='covid_dropdown_icon'>
-        <img src='<?php echo get_template_directory_uri() . '/assets/images/icons/plus_icon_covid.png' ?>'>
     </div>
 </section>
