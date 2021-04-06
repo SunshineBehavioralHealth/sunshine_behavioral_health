b5a4e4d1666b2e68e6ab3cc01d720cee60abecd4
Nathan Wilson <nathanw@monarch.local>
2021-04-06 13:28:41 -0400

    changed spacing for treatment_options

--- template-parts/content/shortcodes/covid19.php
+++ template-parts/content/shortcodes/covid19.php
@@ -2,9 +2,9 @@
     <div class="covid19_shortcode_container">
         <h4>COVID-19 Questions and Concerns</h4>
         <div class="covid_sidebar_content_wrapper">
-            <p>With the current COVID-19 outbreak, many questions surrounding the virus arise. The safety of our staff and patients is the top priority across all of our centers. We have taken the necessary precautions to minimize the risk of exposure and transmission of the Coronavirus to those in our treatment programs, allowing them to focus on their recovery. </p>
-            <p>We are here to answer your questions and alleviate any concerns. Call us at <a class="invocaNumber" href="tel:949-276-2886">949-276-2886</a>.</p>
             <p>Do not allow COVID-19 to stop you from seeking the care you need.</p>
+            <p>We are here to answer your questions and alleviate any concerns. Call us today.</p>
+            <a class="invocaNumber covid_sidebar_cta" href="tel:949-276-2886">ff949-276-2886</a>
         </div>
 
     </div>
