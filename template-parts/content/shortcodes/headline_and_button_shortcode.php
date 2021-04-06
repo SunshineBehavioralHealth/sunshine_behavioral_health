afacbe9c139dace3cce69cd23c2409560782b424
Nathan Wilson <nathanw@monarch.local>
2021-04-06 10:28:58 -0400

    updated treatment optoins headline

--- template-parts/content/shortcodes/headline_and_button_shortcode.php
+++ template-parts/content/shortcodes/headline_and_button_shortcode.php
@@ -1,5 +1,4 @@
 <section class="headline_and_button_sidebar_shortcode_section hide_on_mobile">
     <h4>Not sure how to Pay for Treatment?</h4>
-    <div class="headline_and_button_cta"><a href="/insurance/">Learn More</a></div>
-
+    <div class="headline_and_button_cta"><a href="/insurance/">Check Your Insurance</a></div>
 </section>
