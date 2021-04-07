dce4cfe13d717b577fa0773d7b6358815e385533
Nathan Wilson <natewilson135@gmail.com>
2021-04-07 09:49:35 -0400

    added new sidebar our centers willow image

--- template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
+++ template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
@@ -10,6 +10,6 @@
         <a href="https://sunshinebehavioralhealth.com/our-rehab-centers/mountain-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/assets/images/sidebar/4mountains-side-bar.jpg' ?>" alt="" loading="lazy"></a>
 
 
-        <a href="https://sunshinebehavioralhealth.com/our-rehab-centers/willow-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/assets/images/sidebar/5-willow-side-bar.jpg' ?>" alt="" loading="lazy"></a>
+        <a href="https://sunshinebehavioralhealth.com/our-rehab-centers/willow-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/assets/images/sidebar/willow_our_center_sidebar.jpg' ?>" alt="" loading="lazy"></a>
     </div>
 </section>
