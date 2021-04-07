2748c3b9064ad3472d22281b04ffb67174e4f84f
Nathan Wilson <natewilson135@gmail.com>
2021-04-07 10:36:17 -0400

    new ch anges

--- template-parts/content/sources.php
+++ template-parts/content/sources.php
@@ -2,9 +2,9 @@
 <?php if (get_field('references')) : ?>
     <section class="sources_section">
         <h4>Sources</h4>
-        <img class="sources_plus_icon" src="<?php echo get_template_directory_uri() . '/assets/images/icons/plus_icon_blue.png.png' ?>" alt="">
+        <img class="sources_plus_icon" src="<?php echo get_template_directory_uri() . '/assets/images/icons/plus_icon_blue.png' ?>" alt="">
         <div class="sources_wrapper hide">
             <?php echo get_field('references') ?>
-        </div>
+        </div>1
     </section>
 <?php endif; ?>
