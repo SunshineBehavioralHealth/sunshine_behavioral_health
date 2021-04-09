6ad996a3fb79a5e8e9ad7a085a8f68a6a4bd64c7
Nathan Wilson <natewilson135@gmail.com>
2021-04-09 15:14:43 -0400

    It would help to have the right class name

--- template-parts/javascript/fixedSidebarJs.php
+++ template-parts/javascript/fixedSidebarJs.php
@@ -3,14 +3,16 @@
 <script>
     jQuery(document).ready(function($) {
         $(function() {
-            var fixedSidebar = $(".clearHeader");
+            var fixedSidebar = $(".fixed_sidebar");
             $(window).scroll(function() {
                 var scroll = $(window).scrollTop();
 
                 if (scroll >= 500) {
                     fixedSidebar.addClass("fixedSidebarPositionFixed");
+                    console.log("added class")
                 } else {
                     fixedSidebar.removeClass("fixedSidebarPositionFixed");
+                    console.log("removed class")
                 }
             });
         });
