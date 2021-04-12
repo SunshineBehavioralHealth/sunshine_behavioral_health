7fe7276e32d4618f9d07102ca0d9b25bbd619ad0
Nathan Wilson <natewilson135@gmail.com>
2021-04-12 11:17:52 -0400

    added headline html somehow missed adding it

--- custom_border_fixed_sidebar_guide.php
+++ custom_border_fixed_sidebar_guide.php
@@ -19,12 +19,23 @@ get_template_part('template-parts/javascript/tableOfContents');
 get_template_part('template-parts/javascript/navigationJs');
 get_template_part('template-parts/javascript/fixedSidebarJs');
 
-
-
 ?>
 
 <main id="primary" class="site-main">
 	<div class="page_wrapper">
+		<div class="page_image">
+			<img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
+			<img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
+			<div class="page_mobile_image_text_and_cta_container">
+				<h1><?php echo get_field('page_headline') ?></h1>
+				<p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
+			</div>
+		</div>
+		<div class="hide_on_mobile show_on_tablet page_headlines_desktop">
+			<h1><?php echo get_field('page_headline') ?></h1>
+			<p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
+		</div>
+
 		<!-- Covid Notice -->
 		<?php get_template_part('template-parts/content/mobileCovidNotice'); ?>
 
