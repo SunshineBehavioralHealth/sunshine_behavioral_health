dae2f0d012b48fde90811d0d88fd26eff8c36b0f
Nathan Wilson <natewilson135@gmail.com>
2021-05-11 13:15:06 -0400

    centers cards carousel css

--- template-parts/content/shortcodes/centers_cards_carousel.php
+++ template-parts/content/shortcodes/centers_cards_carousel.php
@@ -4,12 +4,11 @@
 
         for (var i = 0; i < sliders.length; i++) {
             var glide = new Glide(sliders[i], {
-                gap: 0,
+                gap: 15,
                 perView: 4,
-                autoplay: 5000,
+                autoplay: 3000,
                 hoverpause: true,
                 keyboard: true,
-                rewind: true
             });
 
             glide.mount();
@@ -19,6 +18,10 @@
 
 <section class="centers_cards_carousel_section">
     <div class="centers_cards_carousel_container">
+        <?php if (is_page_template('front-page.php')) : ?>
+            <h2 class=""><?php echo get_field('treatment_center_card_headline') ?></h2>
+            <h4><?php echo get_field('treatment_center_card_subheadline') ?></h4>
+        <?php endif; ?>
         <div class="glide centers_cards_carousel">
             <div class="glide__track" data-glide-el="track">
                 <ul class="glide__slides">
@@ -28,13 +31,13 @@
                     ?>
                             <li class="glide__slide">
                                 <div class="centers_cards_indv_card">
-                                    <p><?php echo get_sub_field('state', 'option') ?></p>
+                                    <p class="centers_cards_indv_card_state"><?php echo get_sub_field('state', 'option') ?></p>
                                     <img src="<?php echo get_sub_field('image', 'option')['url'] ?>" alt="">
                                     <a href="<?php echo get_sub_field('link', 'option') ?>">
                                         <h4><?php echo get_sub_field('center_name', 'option') ?></h4>
                                     </a>
-                                    <p><?php echo get_sub_field('center_subheadline', 'option') ?></p>
-                                    <a href="<?php echo get_sub_field('link', 'option') ?>">Learn More</a>
+                                    <p class="centers_cards_indv_card_subheadline"><?php echo get_sub_field('center_subheadline', 'option') ?></p>
+                                    <a href="<?php echo get_sub_field('link', 'option') ?>" class="centers_cards_indv_card_learn_more">Learn More</a>
                                 </div>
                             </li>
                     <?php
@@ -42,19 +45,6 @@
                     endif;
                     ?>
                 </ul>
-            </div>
-            <div class="glide__bullets" data-glide-el="controls[nav]">
-                <?php
-                $centersCardIncrement = -1;
-                if (have_rows('centers_cards_carousel', 'option')) :
-                    while (have_rows('centers_cards_carousel', 'option')) : the_row();
-                ?>
-                        <button class="glide__bullet" data-glide-dir="<?php $centersCardIncrement++ ?>"></button>
-
-                <?php
-                    endwhile;
-                endif;
-                ?>
             </div>
         </div>
     </div>
