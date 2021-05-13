372ca3be054889d7c82131ba0741b260a97b4808
Nathan Wilson <natewilson135@gmail.com>
2021-05-13 14:40:57 -0400

    undid centers cards css

--- template-parts/content/shortcodes/centers_cards_carousel_image.php
+++ template-parts/content/shortcodes/centers_cards_carousel_image.php
@@ -37,7 +37,7 @@
                                     <?php if (have_rows('centers_cards_repeater_images', 'option')) : ?>
                                         <?php while (have_rows('centers_cards_repeater_images', 'option')) : the_row(); ?>
                                             <li class="glide__slide">
-                                                <img class="centers_cards_card_image" src="<?php echo get_sub_field('image', 'option')['url'] ?>" alt="">
+                                                <img src="<?php echo get_sub_field('image', 'option')['url'] ?>" alt="">
                                             </li>
                                         <?php endwhile; ?>
                                     <?php endif; ?>
