6caf8a14925654727fa1590150d12e47e8ac3c60
Nathan Wilson <natewilson135@gmail.com>
2021-05-13 11:44:36 -0400

    should be last css change for state resources home page

--- front-page.php
+++ front-page.php
@@ -359,7 +359,7 @@ if (is_home()) {
 	</section>
 
 	<section class="state_resources_section lazy">
-		<img class="state_resources_background_image" src="<?php echo get_template_directory_uri() . '/assets/images/sunshine_state_resources_background.jpg' ?>" alt="">
+		<img class="state_resources_background_image" src="<?php echo get_template_directory_uri() . '/assets/images/sunshine_state_resources_background.jpg' ?>" alt="" loading="lazy">
 		<div class="state_resources_container">
 			<div class="state_resources_headline">
 				<img loading="lazy" src="<?php echo get_template_directory_uri() . '/assets/images/icons/address_white.png' ?>" alt="">
