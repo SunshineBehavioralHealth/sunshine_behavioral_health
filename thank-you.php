7404e283a19b40741a967c432a52a55a7993275c
Nathan Wilson <natewilson135@gmail.com>
2021-05-17 13:28:42 -0400

    created thank you page template

---
+++ thank-you.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Template Name: Thank You
+ * Template Post Type: Page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package sunshine_behavioral_health
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+get_template_part('template-parts/javascript/navigationJs');
+get_template_part('template-parts/javascript/expandableRowsShortcodeJs');
+
+sunshine_behavioral_health()->print_styles('sunshine_behavioral_health-custom-page-nosidebar', 'sunshine_behavioral_health-content');
+
+$centerValue = get_field('center')
+
+?>
+
+
+
+<main id="primary" class="maxWidth thank_you_page">
+    <div class="hero_banner_container">
+        <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
+        <div class="hero_banner_headlines_container">
+            <h1><?php echo get_field('page_headline') ?></h1>
+            <p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
+        </div>
+
+    </div>
+    <div class="page_grid">
+        <div class="page_wrapper thank_you_page_wrapper">
+            <!-- Page Content -->
+            <section class="page_content list_styling">
+                <?php get_template_part('template-parts/content/googleTranslate'); ?>
+                <?php echo get_field('content') ?>
+            </section>
+
+            <!-- Disclosure -->
+            <section class="page_disclosure_section">
+                <?php get_template_part('template-parts/content/pageDisclaimer'); ?>
+            </section>
+        </div>
+        <?php get_sidebar('responsive'); ?>
+    </div>
+    <div class="thank_you_page_footer">
+        <?php get_template_part('template-parts/content/shortcodes/centers_cards_carousel_image'); ?>
+    </div>
+</main> <?php
+        get_footer();
