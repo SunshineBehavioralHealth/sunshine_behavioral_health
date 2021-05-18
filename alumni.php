4ea1d9e5e3329d90d0938abf8a90eb9b4104266c
Nathan Wilson <natewilson135@gmail.com>
2021-05-18 10:09:34 -0400

    alumni changes

--- alumni.php
+++ alumni.php
@@ -24,12 +24,12 @@ $centerValue = get_field('center')
 
 
 <main id="primary" class="alumni_page">
-    <div class="thank_you_hero_container">
+    <div class="alumni_hero_container desktop_and_mobile_hero_image_wrapper">
         <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
         <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
         <div class="hero_banner_headlines_container">
-            <h1><?php echo get_field('thank_you_headline') ?></h1>
-            <?php if (get_field('page_headline')) : ?>
+            <h1><?php echo get_field('page_headline') ?></h1>
+            <?php if (get_field('page_subheadline_paragraph')) : ?>
                 <p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
             <?php endif; ?>
         </div>
