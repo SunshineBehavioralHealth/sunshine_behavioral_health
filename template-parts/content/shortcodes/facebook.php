2092e99daf7b228b70276ac974f8b9a1981edae9
Nathan Wilson <natewilson135@gmail.com>
2021-05-18 12:33:27 -0400

    moved even columns class

--- template-parts/content/shortcodes/facebook.php
+++ template-parts/content/shortcodes/facebook.php
@@ -1,13 +1,13 @@
 <section class="facebook_shortcode_section">
     <div class="facebook_shortcode_container">
         <h3><?php echo get_field('facebook_-_headline', 'option') ?></h3>
-        <div class="facebook_shortcode_repeater_container">
+        <div class="facebook_shortcode_repeater_container even_columns">
 
             <?php
             if (have_rows('facebook_-_repeater', 'option')) :
                 while (have_rows('facebook_-_repeater', 'option')) : the_row();
             ?>
-                    <div class="facebook_shortcode_repeater_element even_columns">
+                    <div class="facebook_shortcode_repeater_element">
                         <a href="/<?php echo get_sub_field('link', 'option') ?>"><img src="<?php echo get_sub_field('icon', 'option')['url'] ?>" alt=""></a>
                     </div>
 
