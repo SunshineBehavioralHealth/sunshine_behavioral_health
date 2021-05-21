44ba8464c6dd4a23b552177a0f842903948cbdb4
Nathan Wilson <natewilson135@gmail.com>
2021-05-21 10:14:54 -0400

    added css that controls some hero text positioning. Readded if conditional for author because I'm a dingus and dev.sunshine was using the wrong theme at that moment and I had already fixed the php error on that theme

--- full_width_hero_no_sidebar.php
+++ full_width_hero_no_sidebar.php
@@ -27,6 +27,10 @@ $centerValue = get_field('center')
         <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
         <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
         <div class="hero_banner_headlines_container">
+        <?php if(get_field('add_center_logo')) ?>
+        <img src="" alt="">
+
+    <?php endif; ?>
             <h1><?php echo get_field('page_headline') ?></h1>
             <?php if (get_field('page_subheadline_paragraph')) : ?>
                 <p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
