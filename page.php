0d1d8252fe2a7884751c2bf95d556fe45b1d240d
Nathan Wilson <natewilson135@gmail.com>
2021-05-25 12:24:40 -0400

    changed some things around on center hero

--- page.php
+++ page.php
@@ -20,9 +20,6 @@ get_template_part('template-parts/javascript/navigationJs');
 
 ?>
 
-
-
-
 <main id="primary" class="">
 	<div class="hero_banner_container">
 		<img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
@@ -30,13 +27,11 @@ get_template_part('template-parts/javascript/navigationJs');
 			<h1><?php echo get_field('page_headline') ?></h1>
 			<p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
 		</div>
-
 	</div>
 	<div class="page_grid">
 		<div class="page_wrapper">
 			<!-- Covid Notice -->
 			<?php get_template_part('template-parts/content/mobileCovidNotice'); ?>
-
 
 			<!-- Medical Review -->
 			<section class="editor_and_review_section hide_on_desktop">
