4aacadeef1692b9e56b453a3f3f4be73b50cfdff
Nathan Wilson <natewilson135@gmail.com>
2021-05-26 11:04:40 -0400

    added subheadline to facebook per Michelle's reqeust

--- template-parts/content/shortcodes/facebook.php
+++ template-parts/content/shortcodes/facebook.php
@@ -1,6 +1,7 @@
 <section class="facebook_shortcode_section">
     <div class="facebook_shortcode_container">
         <h3><?php echo get_field('facebook_-_headline', 'option') ?></h3>
+        <p><?php echo get_field('facebook_-_subheadline', 'option') ?></p>
         <div class="facebook_shortcode_repeater_container">
 
             <?php
