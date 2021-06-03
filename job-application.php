faf63b22eaff1306bb924570b8e571dc70ee33fc
Nathan Wilson <natewilson135@gmail.com>
2021-06-03 14:47:06 -0400

    job application template updates to new style

--- job-application.php
+++ job-application.php
@@ -20,39 +20,6 @@ sunshine_behavioral_health()->print_styles('sunshine_behavioral_health-custom-pa
 <main id="primary" class="job_application_page maxWidth">
 
     <div class="page_image">
-        <?php if (!wp_is_mobile()) : ?>
-            <?php
-
-            $centerValue = get_field('center');
-
-            if ($centerValue == "Chapters Capistrano") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/career-chapters.jpg' ?>">
-            <?php elseif ($centerValue == "Monarch Shores") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/career-opportunities-monarch.jpg' ?>">
-            <?php elseif ($centerValue == "Mountain Springs") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/career-mountain-springs.jpg' ?>">
-            <?php elseif ($centerValue == "Willow Springs") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/career-opportunities-willow.jpg' ?>">
-            <?php elseif ($centerValue == "Sunshine Behavioral Health") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/career-sunshine.jpg' ?>">
-            <?php endif; ?>
-        <?php else : ?>
-            <?php
-
-            $centerValue = get_field('center');
-
-            if ($centerValue == "Chapters Capistrano") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/chapters-mobile.jpg' ?>">
-            <?php elseif ($centerValue == "Monarch Shores") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/monarch-moblie.jpg' ?>">
-            <?php elseif ($centerValue == "Mountain Springs") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/mountain-springs-mobile.jpg' ?>">
-            <?php elseif ($centerValue == "Willow Springs") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/willow-springs-mobile.jpg' ?>">
-            <?php elseif ($centerValue == "Sunshine Behavioral Health") : ?>
-                <img class="page_desktop_image" src="<?php echo get_template_directory_uri() . '/images/sunshine-mobile.jpg' ?>">
-            <?php endif; ?>
-        <?php endif; ?>
         <div class="page_mobile_image_text_and_cta_container">
             <h1><?php echo get_field('page_headline') ?></h1>
             <?php if (get_field('page_subheadline_paragraph')) : ?>
@@ -63,14 +30,28 @@ sunshine_behavioral_health()->print_styles('sunshine_behavioral_health-custom-pa
 
     <div class="job_application_page_content_container">
         <div>
-            <h2><?php echo get_field('job_title') ?></h2>
-            <div class="job_application_job_content">
-                <?php echo get_field('job_content') ?>
-            </div>
-            <div class="job_application_form_container">
-                <?php echo do_shortcode('[gravityform id="1" title="true" description="true"]') ?>
-            </div>
+
         </div>
     </div>
-</main> <?php
-        get_footer();
+</main>
+
+<main id="primary" class="full_width_hero_no_sidebar">
+    <?php get_template_part('template-parts/heros/desktop_and_mobile_hero_full_width_job_app'); ?>
+
+    <div class="content_container">
+        <div class="page_wrapper">
+            <!-- Page Content -->
+            <section class="page_content list_styling">
+                <h2><?php echo get_field('job_title') ?></h2>
+                <div class="job_application_job_content">
+                    <?php echo get_field('job_content') ?>
+                </div>
+                <div class="job_application_form_container">
+                    <?php echo do_shortcode('[gravityform id="1" title="true" description="true"]') ?>
+                </div>
+            </section>
+        </div>
+    </div>
+</main>
+
+<?php get_footer();
