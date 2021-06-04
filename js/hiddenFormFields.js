88e74d5451acee0d842431d3cf9b537b035a2e3d
Nathan Wilson <natewilson135@gmail.com>
2021-06-04 10:20:23 -0400

    added comments to hiddenFormFields and corrected hero_banner_container CLS issue

--- js/hiddenFormFields.js
+++ js/hiddenFormFields.js
@@ -4,15 +4,14 @@ jQuery(document).ready(function ($) {
         var value = re.exec(document.cookie);
         return (value != null) ? unescape(value[1]) : null;
     }
-
+    // Google Analytics User ID
     let googleAnalyticsUserID = getCookie('_ga');
-    console.log('User ID:', googleAnalyticsUserID);
-
     let googleAnalyticsUserIdInput = $('.google_analytics_id_field  input');
 
     console.log("value of field before changing the value:", googleAnalyticsUserIdInput.val());
-
     googleAnalyticsUserIdInput.val(googleAnalyticsUserID);
 
     console.log('value of field after changing the value:', googleAnalyticsUserIdInput.val());
+
+    // To work with Gravity forms add the class 'google_analytics_id_field' to the form input. See selector above.
 });
