87cb3ba3be4eac5d7e314e4cfd4ad7d575de9852
Nathan Wilson <natewilson135@gmail.com>
2021-06-15 11:03:25 -0400

    updated lincoln image file path in learn_centers_sidebar_shortcode

--- template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
+++ template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
@@ -11,7 +11,7 @@
 
         <a href="/our-rehab-centers/willow-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/willow_sidebar.png' ?>" alt="" loading="lazy"></a>
 
-        <a href="/our-rehab-centers/lincoln-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/learn_centers_sidebar_section' ?>" alt="" loading="lazy"></a>
+        <a href="/our-rehab-centers/lincoln-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/lincoln_sidebar_blue_bottom.png' ?>" alt="" loading="lazy"></a>
 
 
     </div>
