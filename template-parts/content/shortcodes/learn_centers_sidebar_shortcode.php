c273d67fe2f2b40f4740ead9c87bb5250e137fe2
Nathan Wilson <natewilson135@gmail.com>
2021-06-15 10:13:41 -0400

    updated learn_centers_sidebar with lincoln on bottom

--- template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
+++ template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
@@ -9,9 +9,9 @@
 
         <a href="/our-rehab-centers/mountain-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/4mountains-side-bar.jpg' ?>" alt="" loading="lazy"></a>
 
-        <a href="/our-rehab-centers/lincoln-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/lincoln_our_center_sidebar.jpg' ?>" alt="" loading="lazy"></a>
+        <a href="/our-rehab-centers/willow-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/willow_sidebar.png' ?>" alt="" loading="lazy"></a>
 
-        <a href="/our-rehab-centers/willow-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/willow_our_center_sidebar.jpg' ?>" alt="" loading="lazy"></a>
+        <a href="/our-rehab-centers/lincoln-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/learn_centers_sidebar_section' ?>" alt="" loading="lazy"></a>
 
 
     </div>
