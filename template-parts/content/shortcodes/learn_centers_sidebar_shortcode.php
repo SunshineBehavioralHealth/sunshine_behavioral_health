966374acf1833cc17e08b7ee8f6a3fb6a6cc5c93
Nathan Wilson <natewilson135@gmail.com>
2021-06-15 13:51:32 -0400

    Some stuff

--- template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
+++ template-parts/content/shortcodes/learn_centers_sidebar_shortcode.php
@@ -4,15 +4,12 @@
 
         <a href="/our-rehab-centers/chapters-capistrano/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/2chapters-side-bar.jpg' ?>" alt="" loading="lazy"></a>
 
-
         <a href="/our-rehab-centers/monarch-shores/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/3-monarch-side-bar.jpg' ?>" alt="" loading="lazy"></a>
 
-        <a href="/our-rehab-centers/mountain-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/4mountains-side-bar.jpg' ?>" alt="" loading="lazy"></a>
+        <a href="/our-rehab-centers/mountain-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/mountains-side-bar.jpg' ?>" alt="" loading="lazy"></a>
 
         <a href="/our-rehab-centers/willow-springs-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/willow_sidebar.png' ?>" alt="" loading="lazy"></a>
 
         <a href="/our-rehab-centers/lincoln-recovery/"><img src="<?php echo get_template_directory_uri() . '/images/sidebar/lincoln_sidebar_blue_bottom.png' ?>" alt="" loading="lazy"></a>
-
-
     </div>
 </section>
