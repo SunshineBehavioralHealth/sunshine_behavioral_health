d827080f16190f22906447638cd31504edbdfd0e
Nathan Wilson <natewilson135@gmail.com>
2021-06-17 15:39:50 -0400

    stuff

--- template-parts/heros/desktop_and_mobile_hero_full_width_job_app.php
+++ template-parts/heros/desktop_and_mobile_hero_full_width_job_app.php
@@ -1,5 +1,4 @@
 <?php $centerValue = get_field('job_application_center'); ?>
-<h2><?php echo get_field('job_application_center') ?></h2>
 <div class="desktop_and_mobile_hero_image_wrapper">
     <?php if ($centerValue == "Chapters Capistrano") : ?>
         <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('chapters_capistrano_hero_desktop', 'option')['url'] ?>">
@@ -13,6 +12,8 @@
         <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('mountain_springs_hero_desktop', 'option')['url'] ?>">
     <?php elseif ($centerValue == "Sunshine Behavioral Health") : ?>
         <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('sunshine_hero_desktop', 'option')['url'] ?>">
+    <?php else : ?>
+        <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('default_hero_desktop', 'option')['url'] ?>" alt="">
     <?php endif; ?>
 
     <?php if ($centerValue == "Chapters Capistrano") : ?>
@@ -27,6 +28,8 @@
         <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mountain_springs_hero_mobile', 'option')['url'] ?>">
     <?php elseif ($centerValue == "Sunshine Behavioral Health") : ?>
         <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('sunshine_hero_mobile', 'option')['url'] ?>">
+    <?php else : ?>
+        <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('default_hero_mobile', 'option')['url'] ?>" alt="">
     <?php endif; ?>
 
     <div class="hero_banner_headlines_container">
@@ -44,20 +47,19 @@
             <?php elseif ($centerValue == "Sunshine Behavioral Health") : ?>
                 <img class="hero_center_logo" src="<?php echo get_field('sunshine_logo', 'option')['url'] ?>" alt="">
             <?php endif; ?>
-            <h1><?php if ($centerValue == "Chapters Capistrano") : ?>
-                    <h1>Chapters Capistrano Careers</h1>
-                <?php elseif ($centerValue == "Lincoln Recovery") : ?>
-                    <h1>Chapters Capistrano Careers</h1>
-                <?php elseif ($centerValue == "Monarch Shores") : ?>
-                    <h1>Monarch Shores Careers</h1>
-                <?php elseif ($centerValue == "Mountain Springs") : ?>
-                    <h1>Mountain Springs Careers</h1>
-                <?php elseif ($centerValue == "Willow Springs") : ?>
-                    <h1>Willow Springs Careers</h1>
-                <?php elseif ($centerValue == "Sunshine Behavioral Health") : ?>
-                    <h1>Sunshine Behavioral Health Careers</h1>
-                <?php endif; ?>
-            </h1>
+            <?php if ($centerValue == "Chapters Capistrano") : ?>
+                <h1>Chapters Capistrano Careers</h1>
+            <?php elseif ($centerValue == "Lincoln Recovery") : ?>
+                <h1>Lincoln Recovery Careers</h1>
+            <?php elseif ($centerValue == "Monarch Shores") : ?>
+                <h1>Monarch Shores Careers</h1>
+            <?php elseif ($centerValue == "Mountain Springs") : ?>
+                <h1>Mountain Springs Careers</h1>
+            <?php elseif ($centerValue == "Willow Springs") : ?>
+                <h1>Willow Springs Careers</h1>
+            <?php else : ?>
+                <h1>Sunshine Behavioral Health Careers</h1>
+            <?php endif; ?>
             <?php if (get_field('page_subheadline_paragraph')) : ?>
                 <?php if (get_field('page_subheadline_paragraph')) : ?>
                     <p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
