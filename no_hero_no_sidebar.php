4b82bfa71c7dafe2dfb4129734b777f61be3976a
Nathan Wilson <natewilson135@gmail.com>
2021-06-18 10:09:47 -0400

    created no_hero_no_sidebar page template and corrected some php for accordions_with_toc shortcode

---
+++ no_hero_no_sidebar.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Template Name: No Hero No Sidebar
+ * Template Post Type: Page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package sunshine_behavioral_health
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+get_template_part('template-parts/javascript/navigationJs');
+
+sunshine_behavioral_health()->print_styles('sunshine_behavioral_health-custom-page-nosidebar', 'sunshine_behavioral_health-content');
+
+$centerValue = get_field('center')
+
+?>
+
+
+
+<main id="primary" class="full_width_hero_no_sidebar">
+    <div class="content_container">
+        <div class="page_wrapper">
+            <!-- Page Content -->
+            <section class="page_content list_styling ipad_width_restriction">
+                <?php get_template_part('template-parts/content/googleTranslate'); ?>
+                <?php echo get_field('content') ?>
+            </section>
+
+            <?php get_template_part('template-parts/content/pageDisclaimer'); ?>
+
+            <?php get_template_part('template-parts/content/phoneTreatmentCta'); ?>
+        </div>
+    </div>
+</main>
+<?php get_footer();
--- template-parts/content/shortcodes/accordions_with_toc.php
+++ template-parts/content/shortcodes/accordions_with_toc.php
@@ -16,16 +16,19 @@
                     if (have_rows('accordions_with_toc-element_repeater')) :
                         while (have_rows('accordions_with_toc-element_repeater')) : the_row();
                     ?>
-                            <div class="expandable_row_element">
-                                <div class="expandable_row_element_headline_img_wrapper">
-                                    <h2><?php echo get_sub_field('headline') ?></h2>
-                                    <img src="<?php echo get_template_directory_uri() . '/images/icons/plus_icon_blue.png' ?>" alt="" loading="lazy">
+                            <?php if (get_sub_field('is_group_headline')) : ?>
+                                <h2><?php echo get_sub_field('headline_text') ?></h2>
+                            <?php else : ?>
+                                <div class="expandable_row_element">
+                                    <div class="expandable_row_element_headline_img_wrapper">
+                                        <h2><?php echo get_sub_field('headline_text') ?></h2>
+                                        <img src="<?php echo get_template_directory_uri() . '/images/icons/plus_icon_blue.png' ?>" alt="" loading="lazy">
+                                    </div>
+                                    <div class="expandable_row_content hide">
+                                        <p><?php echo get_sub_field('content') ?></p>
+                                    </div>
                                 </div>
-
-                                <div class="expandable_row_content hide">
-                                    <p><?php echo get_sub_field('content') ?></p>
-                                </div>
-                            </div>
+                            <?php endif; ?>
                     <?php
                         endwhile;
                     endif;
