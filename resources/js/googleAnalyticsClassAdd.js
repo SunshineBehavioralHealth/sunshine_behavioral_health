26fc7be278356b01cc9e8900387df30d0ca57590
Nathan Wilson <natewilson135@gmail.com>
2021-06-23 11:59:09 -0400

    removed TOAST after confirming success to class and ID addition

--- resources/js/googleAnalyticsClassAdd.js
+++ resources/js/googleAnalyticsClassAdd.js
@@ -2,13 +2,13 @@
 jQuery(document).ready(function ($) {
     // Adds GTM class to submit button on forms
     $('footer .gform_footer input[type=submit]').addClass('footerContactForm');
-    $('footer .gform_footer input[type=submit]').attr('id', 'footerContactForm_TOAST1');
+    $('footer .gform_footer input[type=submit]').attr('id', 'footerContactForm');
 
     $('.contact_us_container .gform_footer input[type=submit]').addClass('contactPageFormSubmit');
-    $('.contact_us_container .gform_footer input[type=submit]').attr('id', 'contactPageFormSubmit_TOAST2');
+    $('.contact_us_container .gform_footer input[type=submit]').attr('id', 'contactPageFormSubmit');
 
     $('.contact_form_shortcode_section .gform_footer input[type=submit]').addClass('contactFormCta');
-    $('.contact_form_shortcode_section .gform_footer input[type=submit]').attr('id', 'contactFormCta_TOAST3');
+    $('.contact_form_shortcode_section .gform_footer input[type=submit]').attr('id', 'contactFormCta');
 
 });
 
